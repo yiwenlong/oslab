ed2a361494bbeb0e49c12951c392f73839453f8c
yiwenlong <wlong.yi@gmail.com>
2023-09-27 11:03:59 +0800

    Update a demo for compare_and_swap.

--- let05/lock/cas_main.c
+++ let05/lock/cas_main.c
@@ -5,7 +5,9 @@ void compare_and_swap(int *ptr, int expect, int new_val);
 int main() {
     int val = 0;
     compare_and_swap(&val, 0, 1);
+    printf("1. val: %d\n", val);
 
-    printf("val: %d\n", val);
+    compare_and_swap(&val, 0, 2);
+    printf("2. val: %d\n", val);
     return 0;
 }
